ca3dfb32801a52c91d5470d1a460dbd58908d4c6
Jayant Sharma <jsharma1_be19@thapar.edu>
2021-11-24 23:11:15 +0530

    sarthak/Jayant login_logout

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ComplaintForm(forms.ModelForm):
     class Meta:
         model = models.Complaint
         fields = ['computer', 'complaint']
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    name=forms.CharField(required=True)
+    mobile_number=forms.IntegerField(required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1","mobile_number","name", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
