fb0fa74cd78dc38fed54452284dfaf31001c732d
Jayant Sharma <jsharma1_be19@thapar.edu>
2021-11-26 02:49:39 +0530

    complaints woking

--- main/forms.py
+++ main/forms.py
@@ -4,10 +4,8 @@ from .models import Staff
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class ComplaintForm(forms.ModelForm):
-    class Meta:
-        model = models.Complaint
-        fields = ['computer', 'complaint']
+class ComplaintForm(forms.Form):
+    complaint=forms.CharField(max_length=1024)
 
 
 class LoginForm(AuthenticationForm):
@@ -22,9 +20,9 @@ class NewUserForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password Confirm'}))
-    name=forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Full Name'}))
-    email = forms.EmailField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Email'}))  
-    mobile_number=forms.IntegerField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Mobile Number'}))
+    name=forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Full Name'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder':'Email'}))  
+    mobile_number=forms.IntegerField(required=True, widget=forms.TextInput(attrs={'placeholder':'Mobile Number'}))
     class Meta:
         model = User
         fields = ("username", "name", "mobile_number","email", "password1", "password2")
